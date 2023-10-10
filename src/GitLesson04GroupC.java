477a8b405bf5020c3fd36baa5640d1e62e61518f
AndreyKhrypach <pacik78@gmail.com>
2023-10-10 20:38:23 +0300

    add new system out println

--- src/GitLesson04GroupC.java
+++ src/GitLesson04GroupC.java
@@ -1,5 +1,6 @@
 public class GitLesson04GroupC {
     public static void main(String[] args) {
         System.out.println("Hello world and Git! iam Andrey");
+        System.out.println("second commit to git");
     }
 }
